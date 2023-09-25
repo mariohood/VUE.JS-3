237b737c0f92f3a5ee64029d4faae784f0766b91
mariohood <manhattan199@hotmail.com>
2023-09-25 19:58:59 -0300

    Boton Nuevos Gastos - Componente Modal

--- admin-gastos/src/App.vue
+++ admin-gastos/src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
-  import {ref} from 'vue'
+  import { ref, reactive } from 'vue'
   import Presupuesto from "./components/Presupuesto.vue";
   import ControlPresupuesto from "./components/ControlPresupuesto.vue";
+  import Modal from "./components/Modal.vue";
+  import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
+  const modal = reactive ({
+    mostrar: false,
+    animar: false
+  })
   const presupuesto = ref(0)
   const disponible = ref(0)
 
   const definirPresupuesto = (cantidad) => {
     presupuesto.value = cantidad
     disponible.value = cantidad
+  }
+
+  const mostrarModal = () => {
+    modal.mostrar = true
+    setTimeout(() => {
+      modal.animar = true
+    }, 100);
   }
 </script>
 
@@ -29,6 +42,24 @@
           />
         </div>
     </header>
+
+    <main v-if="presupuesto > 0">
+
+
+      <div class="crear-gasto">
+        <img
+          :src="iconoNuevoGasto"
+          alt="icono nuevo gasto"
+          @click="mostrarModal"
+        />
+      </div>
+
+      <Modal
+        v-if="modal.mostrar"
+      />
+    </main>
+
+
   </div>  
 </template>
 
@@ -87,6 +118,15 @@
     border-radius: 1.2rem;
     padding: 5rem;
   }
-  
+
+  .crear-gasto {
+    position: fixed;
+    bottom: 5rem;
+    right: 5rem;
+  }
+  .crear-gasto img {
+    width: 5rem;
+    cursor:pointer;
+  }
 
 </style>
---
+++ admin-gastos/src/components/Modal.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Desde Modal </h1>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
