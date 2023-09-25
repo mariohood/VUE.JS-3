8af85e49a512a41c7836ac04c4b457305c3c9e11
mariohood <manhattan199@hotmail.com>
2023-09-25 18:15:55 -0300

    Tela de Gastos, Disponible, gastado - CSS panel presupuesto

--- admin-gastos/src/components/ControlPresupuesto.vue
+++ admin-gastos/src/components/ControlPresupuesto.vue
@@ -38,6 +38,61 @@
 </template>
 
 
-<style lang="scss" scoped>
+<style scoped>
+    .dos-columnas {
+        display: flex;
+        flex-direction: column;
+    }
+    .dos-columnas > :first-child{
+        margin-bottom: 3rem;
+    }
+
+    @media (min-width: 768px) {
+        .dos-columnas {
+            flex-direction: row;
+            gap: 4rem;
+            align-items: center;
+        }
+        .dos-columnas > :first-child{
+        margin-bottom: 0;
+        }
+    }
+
+    .reset-app {
+        background-color: #DB2777;
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        color: var(--blanco);
+        font-weight: 900;
+        text-transform: uppercase;
+        border-radius: 1rem;
+        transition-property: background-color;
+        transition-duration: 300ms;
+    }
+    .reset-app:hover {
+        cursor: pointer;
+        background-color: #c2226a;
+    }
+    .contenedor-presupuesto {
+        width: 100%;
+
+    }
+
+    .contenedor-presupuesto p {
+        font-size: 2.4rem;
+        text-align: center;
+        color: var(--gris-oscuro);
+    }
+    @media (min-width: 768px) {
+        .contenedor-presupuesto p {
+            text-align: left;
+        }
+    }
+
+    .contenedor-presupuesto span {
+        font-weight: 900;
+        color: var(--azul);
+    }
 
 </style>
